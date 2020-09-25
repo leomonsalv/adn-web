3d1cef8ce12f75c5ba867de27cf252f8f2a9bd17
Chux Rincon <chux.rincon@gmail.com>
2020-09-24 22:30:37 -0400

    Added new changes to app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     'plugin:react/recommended',
     'plugin:sonarjs/recommended',
     'airbnb',
-    'plugin:unicorn/recommended',
+    'plugin:unicorn/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: '2020',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'sonarjs'],
   rules: {
     'react/jsx-filename-extension': [0],
     'unicorn/filename-case': [0],
     'linebreak-style': 0,
+    'comma-dangle': 0
   },
   globals: {
     test: 'readonly',
-    expect: 'readonly',
-  },
+    expect: 'readonly'
+  }
 };
