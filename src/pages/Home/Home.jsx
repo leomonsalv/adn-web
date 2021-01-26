b73c9cd9ba3e034ef7109fd82e4144d420534621
murbina <ma2urbina@gmail.com>
2021-01-25 22:57:47 -0500

    create component finder

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { useWindowWidth } from '@react-hook/window-size';
 import SkelletonWeb from '../../components/Skelleton/SkelletonWeb';
 import SkelletonMovil from '../../components/Skelleton/SkelletonMovil';
+import Finder from '../../components/Finder/Finder';
 
 const Home = () => {
   const onlyWidth = useWindowWidth();
   return (
     <>
+      <Finder />
       {onlyWidth < 600 ? <SkelletonMovil /> : <SkelletonWeb /> }
     </>
   );
