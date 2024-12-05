9f7f7fe1e52e1b11dc7c9ddc51ad3560d20909b0
Juan Carlos Barone Navarro <jbarone@modu.one>
2024-12-05 16:56:01 -0400

    [fix]: added tailwind.ui remote domain for images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 // @ts-check
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["tailwindui.com"],
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -15,15 +12,16 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    domains: ['tailwindui.com'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
