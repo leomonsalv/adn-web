d11763e464e46f87933468f8760be76dc2e478df
Chuxie <crincon@modu.one>
2025-05-26 17:18:41 -0400

    Added sitemap change on behavior

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -5,16 +5,16 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    <sitemap>
-      <loc>https://www.adan.life/page-sitemap.xml</loc>
-      <lastmod>2025-05-22</lastmod>
+      <loc>https://www.adan.life/page-sitemap/sitemap.xml</loc>
+      <lastmod>2025-05-26</lastmod>
    </sitemap>
    <sitemap>
-      <loc>https://www.adan.life/category-sitemap.xml</loc>
-      <lastmod>2025-05-22</lastmod>
+      <loc>https://www.adan.life/category-sitemap/sitemap.xml</loc>
+      <lastmod>2025-05-26</lastmod>
    </sitemap>
    <sitemap>
-      <loc>https://www.adan.life/product-sitemap.xml</loc>
-      <lastmod>2025-05-22</lastmod>
+      <loc>https://www.adan.life/product-sitemap/sitemap.xml</loc>
+      <lastmod>2025-05-26</lastmod>
    </sitemap>
 </sitemapindex>`;
 
